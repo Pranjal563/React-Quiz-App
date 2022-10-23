20bbf527b3a728c691a33b9f25fd0ff0f7342c8d
Pranjal <pbaranwal10062002@gmail.com>
2022-10-23 23:25:19 +0530

    second commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,13 +6,7 @@ const Footer = () => {
         marginBottom: 10,
       }}
     >
-      Made with ♥ by{" "}
-      <a
-        href="https://www.youtube.com/roadsidecoder"
-        style={{ cursor: "pointer" }}
-      >
-        Roadside Coder
-      </a>
+      Made with ❤️ by Pranjal Barnwal
     </div>
   );
 };
